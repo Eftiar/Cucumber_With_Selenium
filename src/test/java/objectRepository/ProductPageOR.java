129fcf2bd3ef62a3e6de74de219995e752ec4f13
Eftiar <eftiar.hossain@monstar-lab.com>
2024-04-04 12:38:48 +0600

    Ecommerce tests added

---
+++ src/test/java/objectRepository/ProductPageOR.java
@@ -0,0 +1,9 @@
+package objectRepository;
+
+import org.openqa.selenium.By;
+
+public class ProductPageOR {
+    public static By addToCartButton = By.className("add-to-cart-buy-now-btn");
+    public static By addedToCartText = By.className("cart-message-text");
+
+}
