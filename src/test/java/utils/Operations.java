67f36fa846f38b51f24583de31a8a9c6a3f5df49
Eftiar <eftiar.hossain@monstar-lab.com>
2024-04-16 12:20:47 +0600

    'Maven updates'

--- src/test/java/utils/Operations.java
+++ src/test/java/utils/Operations.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
@@ -8,7 +9,6 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import java.io.File;
 import java.time.Duration;
